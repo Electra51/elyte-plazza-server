013077c9520cd91f4b7e7709accaf42c0651183a
Electra51 <safayetnurelectra@gmail.com>
2022-11-24 21:13:32 +0600

    category api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -13,12 +13,41 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iohfkju.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 console.log(uri);
 
 async function run() {
     try {
         const categoriesCollection = client.db('icebox').collection('categories');
-        
+        const productsCollection = client.db('icebox').collection('products');
+
+        app.get('/categories', async (req, res) => { 
+         
+            const query = {};
+            const category = await categoriesCollection.find(query).toArray();
+            res.send(category);
+        })
+        app.get('/category/:id', async (req, res) => { 
+            const id = req.params.id;
+            const query = { category_id: id };
+            const product = await productsCollection.find(query).toArray();
+            res.send(product);
+        })
+
+        // app.get('/products/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { category_id: id };
+        //     const product = await productsCollection.find(query).toArray();
+        //     console.log(product);
+        //     res.send(product);
+        // })
+        app.get('/products', async (req, res) => { 
+         
+            const query = {};
+            const product = await productsCollection.find(query).toArray();
+            res.send(product);
+        })
+     
 
     }
     finally {
@@ -26,7 +55,6 @@ async function run() {
     }
 }
 run().catch(console.log);
-
 
 app.get('/', async (req, res) => {
     res.send('icebox server is running');
