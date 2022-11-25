4c3621e7554792f661a655400caefab9d9e5a427
Electra51 <safayetnurelectra@gmail.com>
2022-11-25 18:55:41 +0600

    booking get operation done

--- index.js
+++ index.js
@@ -57,6 +57,14 @@ async function run() {
             res.send(product);
         })
 
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const bookings= await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+            console.log(bookings);
+        })
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             console.log(booking);
