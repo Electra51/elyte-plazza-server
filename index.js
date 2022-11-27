2e2a9673fbe617f0636226b92047aea88f56f005
Electra51 <safayetnurelectra@gmail.com>
2022-11-27 12:51:34 +0600

    delete done

--- index.js
+++ index.js
@@ -229,6 +229,15 @@ async function run() {
             console.log(result)
             res.send(result);
         })
+
+        //delete product
+        app.delete('/addProducts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await addProductsCollection.deleteOne(query);
+            res.send(result);
+        })
+
          //myproduct get
          app.get('/addProducts', async (req, res) => {
              const query = {};
